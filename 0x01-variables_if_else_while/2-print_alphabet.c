0b089f829f21f3de92b00b6466f748dab80a8eb2
beesee005 <beesee005@gmail.com>
2023-06-19 16:12:25 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entrance
+ * Return: Always 0 (Successful)
+ */
 
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
